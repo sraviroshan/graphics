ae00c6adb0912798bbf517e46d250883ac02cd2d
Ashish Sonone <ashson07@gmail.com>
2014-09-29 21:47:08 +0530

    texture tried on torso

--- asgn2_part1/src/transformer.cpp
+++ asgn2_part1/src/transformer.cpp
@@ -23,6 +23,8 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <cstdio>
+#include <cstdlib>
 
 #include "gl_framework.hpp"
 
@@ -66,6 +68,8 @@
 
 #define PI 3.14159265359
 
+GLuint texture[1]; 
+
 //! The pointer to the GLFW window
 GLFWwindow* window;
 
@@ -81,6 +85,132 @@ void unit_weel(void);
 void front_weel_slab(void);
 void weel(void);
 void back_weel_slab(void);
+
+/*************** TEXTURE KACHRA **********/
+/* Image type - contains height, width, and data */
+struct Image {
+    unsigned long sizeX;
+    unsigned long sizeY;
+    char *data;
+};
+typedef struct Image Image;
+
+int ImageLoad(char *filename, Image *image) {
+    FILE *file;
+    unsigned long size;                 // size of the image in bytes.
+    unsigned long i;                    // standard counter.
+    unsigned short int planes;          // number of planes in image (must be 1) 
+    unsigned short int bpp;             // number of bits per pixel (must be 24)
+    char temp;                          // temporary color storage for bgr-rgb conversion.
+
+    // make sure the file is there.
+    if ((file = fopen(filename, "rb"))==NULL)
+    {
+	printf("File Not Found : %s\n",filename);
+	return 0;
+    }
+    
+    // seek through the bmp header, up to the width/height:
+    fseek(file, 18, SEEK_CUR);
+
+    // read the width
+    if ((i = fread(&image->sizeX, 4, 1, file)) != 1) {
+	printf("Error reading width from %s.\n", filename);
+	return 0;
+    }
+    printf("Width of %s: %lu\n", filename, image->sizeX);
+    
+    // read the height 
+    if ((i = fread(&image->sizeY, 4, 1, file)) != 1) {
+	printf("Error reading height from %s.\n", filename);
+	return 0;
+    }
+    printf("Height of %s: %lu\n", filename, image->sizeY);
+    
+    // calculate the size (assuming 24 bits or 3 bytes per pixel).
+    size = image->sizeX * image->sizeY * 3;
+
+    // read the planes
+    if ((fread(&planes, 2, 1, file)) != 1) {
+	printf("Error reading planes from %s.\n", filename);
+	return 0;
+    }
+    if (planes != 1) {
+	printf("Planes from %s is not 1: %u\n", filename, planes);
+	return 0;
+    }
+
+    // read the bpp
+    if ((i = fread(&bpp, 2, 1, file)) != 1) {
+	printf("Error reading bpp from %s.\n", filename);
+	return 0;
+    }
+    if (bpp != 24) {
+	printf("Bpp from %s is not 24: %u\n", filename, bpp);
+	return 0;
+    }
+	
+    // seek past the rest of the bitmap header.
+    fseek(file, 24, SEEK_CUR);
+
+    // read the data. 
+    image->data = (char *) malloc(size);
+    if (image->data == NULL) {
+	printf("Error allocating memory for color-corrected image data");
+	return 0;	
+    }
+
+    if ((i = fread(image->data, size, 1, file)) != 1) {
+	printf("Error reading image data from %s.\n", filename);
+	return 0;
+    }
+
+    for (i=0;i<size;i+=3) { // reverse all of the colors. (bgr -> rgb)
+	temp = image->data[i];
+	image->data[i] = image->data[i+2];
+	image->data[i+2] = temp;
+    unsigned int a = (unsigned char) image->data[i];
+    //printf("%u ", a);
+    }
+    
+    // we're done.
+    return 1;
+}
+    
+// Load Bitmaps And Convert To Textures
+void LoadGLTextures() {	
+    // Load Texture
+    Image *image1;
+    
+    // allocate space for texture
+    image1 = (Image *) malloc(sizeof(Image));
+    if (image1 == NULL) {
+	printf("Error allocating space for image");
+	exit(0);
+    }
+
+    if (!ImageLoad("silver1.bmp", image1)) {
+	exit(1);
+    }        
+
+    // Create Texture	
+    glGenTextures(1, &texture[0]);
+    glBindTexture(GL_TEXTURE_2D, texture[0]);   // 2d texture (x and y size)
+
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR); // scale linearly when image bigger than texture
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); // scale linearly when image smalled than texture
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    unsigned char pixels[] = {
+        0, 0, 0, 255, 255, 255,
+        255, 255, 255, 0, 0, 0
+    };
+    // 2d texture, level of detail 0 (normal), 3 components (red, green, blue), x size from image, y size from image, 
+    // border 0 (normal), rgb color data, unsigned byte data, and finally the data itself.
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, image1->sizeX, image1->sizeY, 0, GL_RGB, GL_UNSIGNED_BYTE, image1->data);
+    //glTexImage2D(GL_TEXTURE_2D, 0, 3, 2, 2, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+};
 
 
 /*-----------------------------INIT DISPLAY LISTS------------------------*/
@@ -243,52 +373,52 @@ void torso(){
   glNewList(id_torso,GL_COMPILE);
   glScalef(torso_xl,torso_yl,torso_zl);
   
-  glColor4f(1, 0, 0, 1);
+  glColor4f(1, 1, 1, 1);
   glBegin(GL_QUADS);            //front face
-  glVertex3f(-1.0f,1.0f,1.0f); 
-  glVertex3f(-1.0f,-1.0f,1.0f);
-  glVertex3f(1.0f,-1.0f,1.0f);
-  glVertex3f(1.0f,1.0f,1.0f);
+  glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,1.0f,1.0f); 
+  glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f,-1.0f,1.0f);
+  glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f,-1.0f,1.0f);
+  glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f,1.0f,1.0f);
   glEnd();
   
-  glColor4f(1, 0, 0, 1);
+  //glColor4f(1, 0, 0, 1);
   glBegin(GL_QUADS);          //back face       
-  glVertex3f(-1.0f,1.0f,-1.0f);
-  glVertex3f(-1.0f,-1.0f,-1.0f);
-  glVertex3f(1.0f,-1.0f,-1.0f);
-  glVertex3f(1.0f,1.0f,-1.0f);
+  glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,1.0f,-1.0f);
+  glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f,-1.0f,-1.0f);
+  glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f,-1.0f,-1.0f);
+  glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f,1.0f,-1.0f);
   glEnd();
   
-  glColor4f(0, 1,0, 1);
+  //glColor4f(0, 1,0, 1);
   glBegin(GL_QUADS);          //left face
-  glVertex3f(-1.0f,1.0f,1.0f);
-  glVertex3f(-1.0f,1.0f,-1.0f);
-  glVertex3f(-1.0f,-1.0f,-1.0f);
-  glVertex3f(-1.0f,-1.0f,1.0f);
+  glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,1.0f,1.0f);
+  glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f,1.0f,-1.0f);
+  glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,-1.0f,-1.0f);
+  glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,-1.0f,1.0f);
   glEnd();
 
-  glColor4f(0, 1, 0, 1);
+  //glColor4f(0, 1, 0, 1);
   glBegin(GL_QUADS);          //right face
-  glVertex3f(1.0f,1.0f,1.0f);
-  glVertex3f(1.0f,1.0f,-1.0f);
-  glVertex3f(1.0f,-1.0f,-1.0f);
-  glVertex3f(1.0f,-1.0f,1.0f);
+  glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f,1.0f,1.0f);
+  glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f,1.0f,-1.0f);
+  glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f,-1.0f,-1.0f);
+  glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f,-1.0f,1.0f);
   glEnd();
   
-  glColor4f(0, 0, 1, 1);
+  //glColor4f(0, 0, 1, 1);
   glBegin(GL_QUADS);        //top face        
-  glVertex3f(-1.0f,1.0f,-1.0f);
-  glVertex3f(-1.0f,1.0f,1.0f);
-  glVertex3f(1.0f,1.0f,1.0f);
-  glVertex3f(1.0f,1.0f,-1.0f);
+  glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,1.0f,-1.0f);
+  glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f,1.0f,1.0f);
+  glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f,1.0f,1.0f);
+  glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f,1.0f,-1.0f);
   glEnd();
   
-  glColor4f(0, 0,1, 1);
+  //glColor4f(0, 0,1, 1);
   glBegin(GL_QUADS);        //bottom face
-  glVertex3f(-1.0f,-1.0f,-1.0f);
-  glVertex3f(-1.0f,-1.0f,1.0f);
-  glVertex3f(1.0f,-1.0f,1.0f);
-  glVertex3f(1.0f,-1.0f,-1.0f);
+  glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,-1.0f,-1.0f);
+  glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f,-1.0f,1.0f);
+  glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f,-1.0f,1.0f);
+  glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f,-1.0f,-1.0f);
   glEnd();
 
 
@@ -611,7 +741,8 @@ void hierarchi(){
       
 
     glPushMatrix();
-      glColor4f(1.0,0.0,0.0,1.0);
+      //glColor4f(1.0,0.0,0.0,1.0);
+      glBindTexture(GL_TEXTURE_2D, texture[0]);   // choose the texture to use.
       glCallList(id_torso);
     glPopMatrix();
     //weel
@@ -806,8 +937,19 @@ int main (int argc, char *argv[])
 
 
   glEnable(GL_DEPTH_TEST);
+  glEnable(GL_TEXTURE_2D); //Enable texture
+
+
   //Initialize GL state
   csX75::initGL();
+  //Generate texture
+  LoadGLTextures();				// Load The Texture(s) 
+    glEnable(GL_TEXTURE_2D);			// Enable Texture Mapping
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);	// Clear The Background Color To Black
+    glClearDepth(1.0);				// Enables Clearing Of The Depth Buffer
+    glDepthFunc(GL_LESS);			// The Type Of Depth Test To Do
+    glEnable(GL_DEPTH_TEST);			// Enables Depth Testing
+    glShadeModel(GL_SMOOTH);			// Enables Smooth Color Shading
   //csX75::rotation_angle=0;
   csX75::body_rotation_x=0;
   csX75::body_rotation_y=0;
